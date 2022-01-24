c7b6b456b134c9d2f5e6716f1d94588732ad372e
lumsys <sunday4oged@yahoo.com>
2022-01-24 16:42:05 +0100

    user/client relationship

--- app/client.php
+++ app/client.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class client extends Model
 {
     //
-
-    public function client()
-     {
-         return $this->hasOne(client::class, 'clientId', 'id');
-     }
+    public function user()
+    {
+        return $this->hasOne(client::class, 'clientId', 'id');
+    }
+    
 }
